05bb30facc082d42cc7af25d1a711653ea1673b3
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-11-15 07:39:26 +0100

    remove demo and add new title

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -18,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual("Merchant's Guide to the Galaxy");
   });
 
   it('should render title', () => {
@@ -26,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome frontend'
+      "MERCHANT'S GUIDE TO THE GALAXY"
     );
   });
 });
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "Merchant's Guide to the Galaxy";
 }
