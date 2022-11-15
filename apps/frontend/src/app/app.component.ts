4d38e60c1f7b49fcf0b31495215cdd81bf13eda9
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-11-15 07:16:54 +0100

    Initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'merchants-guide-to-the-galaxy-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
