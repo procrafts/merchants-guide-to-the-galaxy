a7c5cec2e2ae96b186b89a8e4d44398fbc85d139
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-11-15 08:49:17 +0100

    add textfield and submit button

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -1,9 +1,24 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+      ],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -27,5 +42,21 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain(
       "MERCHANT'S GUIDE TO THE GALAXY"
     );
+  });
+
+  it('should submit user input', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    const inputHarness = await loader.getHarness(MatInputHarness);
+    const buttonHarness = await loader.getHarness(MatButtonHarness);
+    const submitMock = jest.fn();
+    fixture.componentInstance.submit = submitMock;
+
+    await inputHarness.setValue('glob is I');
+    await buttonHarness.click();
+
+    expect(submitMock).toBeCalled();
+    expect(fixture.componentInstance.commandControl.value).toBe('glob is I');
   });
 });
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
