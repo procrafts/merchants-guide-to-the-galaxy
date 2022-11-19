61be3dc6ef4038f279af598ed7fe7813f9f1f06d
Franz Fabian Schmid <ff.schmid@gmail.com>
2022-11-19 10:51:02 +0100

    add complete challenge test

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,7 +1,8 @@
 import {
+  enterCommand,
+  getAllOutputs,
   getFirstOutput,
-  getInput,
-  getSubmitButton,
+  getNthOutput,
   getTitle,
 } from '../support/app.po';
 
@@ -9,38 +10,62 @@ describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getTitle().contains("MERCHANT'S GUIDE TO THE GALAXY");
+    getTitle().should('contain.text', "MERCHANT'S GUIDE TO THE GALAXY");
   });
 
   it('should profess incomprehension', () => {
-    getInput().type(
+    enterCommand(
       'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
     );
-    getSubmitButton().click();
-    getFirstOutput().contains('I have no idea what you are talking about');
+
+    getFirstOutput().should(
+      'contain.text',
+      'I have no idea what you are talking about'
+    );
   });
 
   it('should count alien numerals', () => {
-    getInput().type('glob is I');
-    getSubmitButton().click();
-    getInput().type('pish is X');
-    getSubmitButton().click();
-    getInput().type('tegj is L');
-    getSubmitButton().click();
-    getInput().type('how much is pish tegj glob glob ?');
-    getSubmitButton().click();
-    getFirstOutput().contains('pish tegj glob glob is 42');
+    enterCommand('glob is I');
+    enterCommand('pish is X');
+    enterCommand('tegj is L');
+    enterCommand('how much is pish tegj glob glob ?');
+
+    getFirstOutput().should('contain.text', 'pish tegj glob glob is 42');
   });
 
   it('should calculate price', () => {
-    getInput().type('glob is I');
-    getSubmitButton().click();
-    getInput().type('prok is V');
-    getSubmitButton().click();
-    getInput().type('glob glob Silver is 34 Credits');
-    getSubmitButton().click();
-    getInput().type('how many Credits is glob prok Silver ?');
-    getSubmitButton().click();
-    getFirstOutput().contains('glob prok Silver is 68 Credits');
+    enterCommand('glob is I');
+    enterCommand('prok is V');
+    enterCommand('glob glob Silver is 34 Credits');
+    enterCommand('how many Credits is glob prok Silver ?');
+
+    getFirstOutput().should('contain.text', 'glob prok Silver is 68 Credits');
+  });
+
+  it('should handle challenge input correctly', () => {
+    enterCommand('glob is I');
+    enterCommand('prok is V');
+    enterCommand('pish is X');
+    enterCommand('tegj is L');
+    enterCommand('glob glob Silver is 34 Credits');
+    enterCommand('glob prok Gold is 57800 Credits');
+    enterCommand('pish pish Iron is 3910 Credits');
+    enterCommand('how much is pish tegj glob glob ?');
+    enterCommand('how many Credits is glob prok Silver ?');
+    enterCommand('how many Credits is glob prok Gold ?');
+    enterCommand('how many Credits is glob prok Iron ?');
+    enterCommand(
+      'how much wood could a woodchuck chuck if a woodchuck could chuck wood ?'
+    );
+
+    getNthOutput(1).should('contain.text', 'pish tegj glob glob is 42');
+    getNthOutput(2).should('contain.text', 'glob prok Silver is 68 Credits');
+    getNthOutput(3).should('contain.text', 'glob prok Gold is 57800 Credits');
+    getNthOutput(4).should('contain.text', 'glob prok Iron is 782 Credits');
+    getNthOutput(5).should(
+      'contain.text',
+      'I have no idea what you are talking about'
+    );
+    getAllOutputs().should('have.lengthOf', 5);
   });
 });
--- apps/frontend-e2e/src/support/app.po.ts
+++ apps/frontend-e2e/src/support/app.po.ts
@@ -1,5 +1,11 @@
 export const getTitle = () => cy.get('h1');
 export const getInput = () => cy.get('[data-testid="input"]');
 export const getSubmitButton = () => cy.get('[data-testid="submit"]');
+export const enterCommand = (text: string) => {
+  getInput().type(text);
+  getSubmitButton().click();
+};
 export const getFirstOutput = () => cy.get('[data-testid="output"] > p:first');
 export const getAllOutputs = () => cy.get('[data-testid="output"] > p');
+export const getNthOutput = (n: number) =>
+  cy.get(`[data-testid="output"] > p:nth-of-type(${n})`);
